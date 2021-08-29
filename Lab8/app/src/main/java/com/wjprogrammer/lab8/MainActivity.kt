d68f87232a20c929f4b1bc0c1834b6e793057d10
jay_wu <jay_wu>
2021-08-29 12:10:02 +0800

    [Lab8] create adapter

--- Lab8/app/src/main/java/com/wjprogrammer/lab8/MainActivity.kt
+++ Lab8/app/src/main/java/com/wjprogrammer/lab8/MainActivity.kt
@@ -9,3 +9,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+//設計新的類別定義聯絡人的資料結構
+data class Contact (
+    val name: String, //姓名
+    val phone: String //電話
+)
