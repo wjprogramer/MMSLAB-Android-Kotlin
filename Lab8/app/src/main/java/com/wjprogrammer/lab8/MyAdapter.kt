a73ae3e6b902795faac24275d8bb0a7baae36c91
jay_wu <jay_wu>
2021-08-29 12:08:33 +0800

    [Lab8] add files and layouts

---
+++ Lab8/app/src/main/java/com/wjprogrammer/lab8/MyAdapter.kt
@@ -0,0 +1,4 @@
+package com.wjprogrammer.lab8
+
+class MyAdapter {
+}
---
+++ Lab8/app/src/main/java/com/wjprogrammer/lab8/SecActivity.kt
@@ -0,0 +1,11 @@
+package com.wjprogrammer.lab8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sec)
+    }
+}
