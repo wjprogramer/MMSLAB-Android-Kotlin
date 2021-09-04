7db37c161ad37727318fcc18f036dd3af49035ce
jay_wu <jay_wu>
2021-09-04 13:09:54 +0800

    [Lab12] create files and layout

---
+++ Lab12/app/src/main/java/com/wjprogrammer/lab12/MyService.kt
@@ -0,0 +1,12 @@
+package com.wjprogrammer.lab12
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
